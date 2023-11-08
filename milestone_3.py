d438dde525401594a7e9f3f3421bc66b3aff0e80
brosefzai <brosefzai@gmail.com>
2023-11-08 05:56:26 +0400

    Created the first two functions (ask_for_input() and check_guess()), and have one call the other

--- milestone_3.py
+++ milestone_3.py
@@ -5,11 +5,22 @@ Created on Wed Nov  8 05:23:18 2023
 
 @author: frasier
 """
+word = "apple"
 
-#Milestone 3, Task 1
-while True:
-    guess = input("Enter a letter: ")
-    if len(guess) == 1 & guess.isalpha():
-        print("Good guess!")
-        break
-    else: print("Invalid letter. Please, enter a single alphabetical character.")
+#Task 3
+
+def ask_for_input():
+    while True:
+        guess = input("Enter a letter: ")
+        if len(guess) == 1 & guess.isalpha():
+            print("Good guess!")
+            break
+        else: print("Invalid letter. Please, enter a single alphabetical character.")
+    
+    check_guess(guess)
+    
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else: print(f"Sorry, {guess} is not in the word. Try again.")
