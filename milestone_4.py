b4002b25eecf987ff644dac071c985cb088fe972
brosefzai <brosefzai@gmail.com>
2023-11-09 05:05:33 +0400

    Added milestone_4.py

---
+++ milestone_4.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  8 06:00:59 2023
+
+@author: frasier
+"""
+import random
+#word = "apple"
+#list_of_guesses = []
+
+class Hangman:
+    
+    def __init__(self, word_list, num_lives=5):
+        self.word = random.choice(word_list)
+        self.word_guessed = [" _ " for x in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
+        
+    def ask_for_input(self):
+        while True:
+            guess = input("Enter a letter: ")
+            if guess.isalpha() == False or len(guess) != 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+        
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for letter in self.word:
+                if letter == guess:
+                    self.word_guessed[(self.word.index(letter))] = guess
+        self.num_lives -= 1
+            
+    
